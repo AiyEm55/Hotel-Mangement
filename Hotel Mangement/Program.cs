2217ac1219918a08c1b4f69d3d01238885e378b7
AiyEm55 <souksavat.am@gmail.com>
2021-04-24 10:27:28 -0700

    Add project files.

---
+++ Hotel Mangement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hotel_Mangement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
